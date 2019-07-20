bc1583f3c79da22f43748448ad81b2ef9c819c02
Shrawan Thapa <shrawan.thapa0174@gmail.com>
2019-07-21 03:01:11 +0545

    updated similar items function

---
+++ Backend/Utilities.py
@@ -0,0 +1,17 @@
+from gensim.models import FastText
+from app import db
+
+
+
+def get_similar_items(item):
+    model = FastText.load("lf.model")
+    similar = model.most_similar(item, topn = 5)
+    item = [item]
+    model.build_vocab([item], update=True)
+    print(model.wv.vocab)
+    model.train([item], total_examples=len([item]), epochs=model.epochs)
+    model.save('lf.model')
+    return similar
+
+if __name__ == "__main__":
+    print(get_similar_items("Samsung S9"))
